fa317db2e80c9ec906c6f3a9a5fc4df4b9fe4771
Oliver Schafeld <schafeld@nexxos.de>
2015-03-17 19:49:35 +0100

    added concat, lint, watch tasks

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # gulp-boilerplate
 Gulp boilerplate (Foundation) script for website projects.
+
+
+#Installation
+git clone {this repository}
+npm install
+npm install gulp-changed --save-dev
+
+#Sources, Docs
+http://mo.phlow.de/gulp/
+http://travismaynard.com/writing/getting-started-with-gulp
--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,11 @@ var changed = require('gulp-changed'),
     clean = require('gulp-clean'),
     minifyhtml = require ('gulp-minify-html'),
     autoprefixer = require ('gulp-autoprefixer'),
-    minifyCSS = require ('gulp-minify-css');
+    minifyCSS = require ('gulp-minify-css'),
+    sass = require('gulp-sass');
 
 
-// Komprimiere Bilder
+// Compress images
 gulp.task('images', function() {
   var imgSrc = './src/images/**/*',
       imgDst = './build/images';
@@ -29,3 +30,36 @@ gulp.task('images', function() {
     .pipe(imagemin())
     .pipe(gulp.dest(imgDst));
 });
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('./src/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('./src/js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('./src/js/*.js', ['lint', 'scripts']);
+    gulp.watch('scss/*.scss', ['sass']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
