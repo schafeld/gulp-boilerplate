f2e708769e1145b84963746324c54c3f19e4cba2
Oliver Schafeld <schafeld@nexxos.de>
2015-04-08 22:23:03 +0200

    added plumber error handling, improved sass task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,3 @@
-// Basiert auf: http://mo.phlow.de/gulp/
-// Bilder optimieren:
-// gulp images
-
-
 // require gulp.js
 var gulp = require('gulp');
 
@@ -26,6 +21,11 @@ var changed = require('gulp-changed'),
 var tsPath = './src/ts/*.ts',
     compilePath = './src/js/compiled',
     dist = 'js/dist';
+
+// Use plumber for error output to console:
+var onError = function(err) {
+    console.log(err);
+}
 
 // Compress TypeScript scripts
 gulp.task('compressScripts', function() {
@@ -56,6 +56,9 @@ gulp.task('images', function() {
       imgDst = './build/images';
 
   gulp.src(imgSrc)
+    .pipe(plumber({
+        errorHandler: onError
+    }))
     .pipe(changed(imgDst))
     .pipe(imagemin())
     .pipe(gulp.dest(imgDst));
@@ -64,20 +67,31 @@ gulp.task('images', function() {
 // Lint Task
 gulp.task('lint', function() {
     return gulp.src('./src/js/*.js')
+        .pipe(plumber({
+            errorHandler: onError
+        }))
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // Compile Our Sass
 gulp.task('sass', function() {
-    return gulp.src('scss/*.scss')
+  var sassSrc = 'src/scss/*.scss',
+      sassDst = 'src/css/';
+    return gulp.src(sassSrc)
+        .pipe(plumber({
+            errorHandler: onError
+        }))
         .pipe(sass())
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest(sassDst));
 });
 
 // Minify CSS
 gulp.task('style', function() {
     return gulp.src('src/css/**/*.css')
+        .pipe(plumber({
+            errorHandler: onError
+        }))
         .pipe(minifyCSS())
         .pipe(gulp.dest('./build/css'));
 });
@@ -85,6 +99,9 @@ gulp.task('style', function() {
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
     return gulp.src('./src/js/*.js')
+        .pipe(plumber({
+            errorHandler: onError
+        }))
         .pipe(concat('all.js'))
         .pipe(gulp.dest('dist'))
         .pipe(rename('all.min.js'))
@@ -95,7 +112,7 @@ gulp.task('scripts', function() {
 // Watch Files For Changes
 gulp.task('watch', function() {
     gulp.watch('./src/js/*.js', ['lint', 'scripts']);
-    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('./src/scss/*.scss', ['sass']);
     gulp.watch('./src/css/*.css', ['style']),
     gulp.watch([tsPath], ['typescript']);
 });
